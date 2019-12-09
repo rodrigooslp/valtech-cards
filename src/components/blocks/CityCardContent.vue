9bcaea7c147ca29e3e486e30d889b6f419baaed2
Rodrigo Lopes <rodrigolopes@codilo.com.br>
2019-12-09 01:14:18 -0300

    Creation of the city card header block

--- src/components/blocks/CityCardContent.vue
+++ src/components/blocks/CityCardContent.vue
@@ -2,12 +2,9 @@
 <div class="flex flex-col justify-center items-center h-full relative" @mouseenter="showEnterAnimation" @mouseleave="showLeaveAnimation">
   <card-overlay ref="card" class="absolute"></card-overlay>
 
-  <div ref="title" class="flex flex-col justify-center items-center mt-8 mb-2">
-    <card-title class="mb-2">{{ title }}</card-title>
-    <card-subtitle>{{ subtitle }}</card-subtitle>
-  </div>
-
+  <city-card-header ref="header" class="mt-8 mb-2" :title="title" :subtitle="subtitle"></city-card-header>
   <card-description ref="description" class="max-h-0 opacity-0">{{ text }}</card-description>
+
   <card-button ref="button" class="mt-3 mb-6 opacity-0 invisible">Explore More</card-button>
 </div>
 </template>
@@ -16,10 +13,9 @@
 import { gsap } from 'gsap'
 
 import CardOverlay from '@/components/elements/CardOverlay'
-import CardTitle from '@/components/elements/CardTitle'
-import CardSubtitle from '@/components/elements/CardSubtitle'
 import CardDescription from '@/components/elements/CardDescription'
 import CardButton from '@/components/elements/CardButton'
+import CityCardHeader from '@/components/blocks/CityCardHeader'
 
 export default {
   props: {
@@ -35,18 +31,17 @@ export default {
   },
   components: {
     CardOverlay,
-    CardTitle,
-    CardSubtitle,
     CardDescription,
-    CardButton
+    CardButton,
+    CityCardHeader
   },
   mounted () {
-    const { title, description, button, card } = this.$refs
+    const { header, description, button, card } = this.$refs
     const timeline = gsap.timeline({ paused: true })
 
     timeline
       .add().to(card.$el, { backgroundImage: 'linear-gradient(#525252bf, #525252bf)', duration: 0.3 })
-      .add().to(title, { y: '-0.5rem', duration: 0.3, ease: 'power3.out' }, '-=0.3')
+      .add().to(header.$el, { y: '-0.5rem', duration: 0.3, ease: 'power3.out' }, '-=0.3')
       .add().to(description.$el, { maxHeight: '100%', opacity: 1, duration: 0.8, ease: 'sin.out' }, '-=0.2')
       .add().to(button.$el, { opacity: 1, visibility: 'visible', duration: 0.2, ease: 'sin.out' }, '-=0.7')
 
---
+++ src/components/blocks/CityCardHeader.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="flex flex-col justify-center items-center">
+  <card-title class="mb-2">{{ title }}</card-title>
+  <card-subtitle>{{ subtitle }}</card-subtitle>
+</div>
+</template>
+
+<script>
+import CardTitle from '@/components/elements/CardTitle'
+import CardSubtitle from '@/components/elements/CardSubtitle'
+
+export default {
+  props: {
+    title: {
+      type: String
+    },
+    subtitle: {
+      type: String
+    }
+  },
+  components: {
+    CardTitle,
+    CardSubtitle
+  }
+}
+</script>
