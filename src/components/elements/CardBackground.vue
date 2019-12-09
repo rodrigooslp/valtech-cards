60bd968f06a41555045184c962561b4b5a577580
Rodrigo Lopes <rodrigolopes@codilo.com.br>
2019-12-08 21:47:13 -0300

    Moved card background styles

--- src/components/elements/CardBackground.vue
+++ src/components/elements/CardBackground.vue
@@ -11,3 +11,77 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.webp {
+  .bg-default {
+    z-index: -1;
+    background-image: url('../../assets/img/bg-default.webp');
+  }
+
+  .bg-venice {
+    z-index: -1;
+    background-image: url('../../assets/img/bg-venice.webp');
+  }
+
+  .bg-venice {
+    z-index: -1;
+    background-image: url('../../assets/img/bg-venice.webp');
+  }
+
+  .bg-berlin {
+    z-index: -1;
+    background-image: url('../../assets/img/bg-berlin.webp');
+  }
+
+  .bg-barcelona {
+    z-index: -1;
+    background-image: url('../../assets/img/bg-barcelona.webp');
+  }
+
+  .bg-paris {
+    z-index: -1;
+    background-image: url('../../assets/img/bg-paris.webp');
+  }
+
+  .bg-amsterdam {
+    z-index: -1;
+    background-image: url('../../assets/img/bg-amsterdam.webp');
+  }
+
+  .bg-london {
+    z-index: -1;
+    background-image: url('../../assets/img/bg-london.webp');
+  }
+}
+
+.no-webp {
+  .bg-default {
+    background-image: url('../../assets/img/bg-default.jpg');
+  }
+
+  .bg-venice {
+    background-image: url('../../assets/img/bg-venice.jpg');
+  }
+
+  .bg-berlin {
+    background-image: url('../../assets/img/bg-berlin.jpg');
+  }
+
+  .bg-barcelona {
+    background-image: url('../../assets/img/bg-barcelona.jpg');
+  }
+
+  .bg-paris {
+    background-image: url('../../assets/img/bg-paris.jpg');
+  }
+
+  .bg-amsterdam {
+    background-image: url('../../assets/img/bg-amsterdam.jpg');
+  }
+
+  .bg-london {
+    background-image: url('../../assets/img/bg-london.jpg');
+  }
+}
+</style>
