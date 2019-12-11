a1bfcea238c95fc063c06aa8c30a9ccef2367d1e
Rodrigo Lopes <rodrigolopes@codilo.com.br>
2019-12-11 17:36:29 -0300

    Implemented purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./src/**/*.html', './src/**/*.vue'],
+  whitelistPatterns: [/^card__background/],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
 module.exports = {
   'plugins': [
     require('tailwindcss')('tailwind.config.js'),
-    require('autoprefixer')()
+    require('autoprefixer')(),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
   ]
 }
