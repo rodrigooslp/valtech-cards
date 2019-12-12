686adba44ea95c2a02ecb2daadf49e0ed297b25f
Rodrigo Lopes <rodrigolopes@codilo.com.br>
2019-12-12 15:06:06 -0300

    Reworked demo folder structure

--- fractal.js
+++ fractal.js
@@ -4,6 +4,6 @@ const path = require('path')
 const fractal = module.exports = require('@frctl/fractal').create()
 
 fractal.set('project.title', 'valtech-cards')
-fractal.components.set('path', path.join(__dirname, 'demo'))
-fractal.docs.set('path', path.join(__dirname, 'docs'))
+fractal.components.set('path', path.join(__dirname, 'demo/components'))
+fractal.docs.set('path', path.join(__dirname, 'demo/docs'))
 fractal.web.set('static.path', path.join(__dirname, 'demo/public'))
