048b20f43dfcba692346d0342a496b35370c6c5a
Rodrigo Lopes <rodrigolopes@codilo.com.br>
2019-12-14 03:19:53 -0300

    Creation of the card title test

---
+++ tests/e2e/specs/components/card-title.spec.js
@@ -0,0 +1,31 @@
+import CardTitle from '../../../../src/components/elements/CardTitle.vue'
+const mountVue = require('cypress-vue-unit-test')
+
+describe('CardTitle', () => {
+  const template = `
+    <div id="app" style="background: gray;">
+      <card-title>{{ text }}</card-title>
+    </div>
+  `
+  const components = { CardTitle }
+  const data = { text: null }
+
+  beforeEach(mountVue({ template, data, components }))
+
+  it('the passed text value should be inside the component', () => {
+    Cypress.vue.text = 'FRONT-END'
+
+    cy
+      .get('h2')
+      .invoke('text')
+      .should('eq', 'FRONT-END')
+  })
+
+  it('the component should not be visible if no text is passed', () => {
+    Cypress.vue.text = ''
+
+    cy
+      .get('h2')
+      .should('not.be.visible')
+  })
+})
