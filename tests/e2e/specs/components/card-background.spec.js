b91abf473132f5dec75d4a5909c0d3df2b6df8d0
Rodrigo Lopes <rodrigolopes@codilo.com.br>
2019-12-14 02:46:53 -0300

    Created the card background test

---
+++ tests/e2e/specs/components/card-background.spec.js
@@ -0,0 +1,107 @@
+import CardBackground from '../../../../src/components/elements/CardBackground.vue'
+const mountVue = require('cypress-vue-unit-test')
+
+describe('CardBackground', () => {
+  const template = `
+    <div id="app">
+      <card-background :image="image" />
+    </div>
+  `
+  const components = { CardBackground }
+  const data = { image: null }
+
+  beforeEach(mountVue({ template, data, components }))
+
+  it('default should be a valid image attribute', () => {
+    Cypress.vue.image = 'default'
+
+    cy
+      .get('img')
+      .invoke('attr', 'src')
+      .should('include', 'default')
+  })
+
+  it('venice should be a valid image attribute', () => {
+    Cypress.vue.image = 'venice'
+
+    cy
+      .get('img')
+      .invoke('attr', 'src')
+      .should('include', 'venice')
+  })
+
+  it('berlin should be a valid image attribute', () => {
+    Cypress.vue.image = 'berlin'
+
+    cy
+      .get('img')
+      .invoke('attr', 'src')
+      .should('include', 'berlin')
+  })
+
+  it('barcelona should be a valid image attribute', () => {
+    Cypress.vue.image = 'barcelona'
+
+    cy
+      .get('img')
+      .invoke('attr', 'src')
+      .should('include', 'barcelona')
+  })
+
+  it('paris should be a valid image attribute', () => {
+    Cypress.vue.image = 'paris'
+
+    cy
+      .get('img')
+      .invoke('attr', 'src')
+      .should('include', 'paris')
+  })
+
+  it('amsterdam should be a valid image attribute', () => {
+    Cypress.vue.image = 'amsterdam'
+
+    cy
+      .get('img')
+      .invoke('attr', 'src')
+      .should('include', 'amsterdam')
+  })
+
+  it('london should be a valid image attribute', () => {
+    Cypress.vue.image = 'london'
+
+    cy
+      .get('img')
+      .invoke('attr', 'src')
+      .should('include', 'london')
+  })
+
+  it('any empty or invalid value should fallback to default', () => {
+    Cypress.vue.image = null
+
+    cy
+      .get('img')
+      .invoke('attr', 'src')
+      .should('include', 'default')
+
+    Cypress.vue.image = undefined
+
+    cy
+      .get('img')
+      .invoke('attr', 'src')
+      .should('include', 'default')
+
+    Cypress.vue.image = ''
+
+    cy
+      .get('img')
+      .invoke('attr', 'src')
+      .should('include', 'default')
+
+    Cypress.vue.image = 'somewhere'
+
+    cy
+      .get('img')
+      .invoke('attr', 'src')
+      .should('include', 'default')
+  })
+})
