8bdf0fe27846e98fb19820373c54e13e8ded38ae
Rodrigo Lopes <rodrigolopes@codilo.com.br>
2019-12-14 02:45:51 -0300

    Updated cypress config

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -1,19 +1,34 @@
-// https://docs.cypress.io/guides/guides/plugins-guide.html
+const webpack = require('@cypress/webpack-preprocessor')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
-// if you need a custom webpack configuration you can uncomment the following import
-// and then use the `file:preprocessor` event
-// as explained in the cypress docs
-// https://docs.cypress.io/api/plugins/preprocessors-api.html#Examples
+const webpackOptions = {
+  resolve: {
+    extensions: ['.js', '.vue']
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['css-loader', 'sass-loader']
+      }
+    ]
+  }
+}
 
-/* eslint-disable import/no-extraneous-dependencies, global-require, arrow-body-style */
-// const webpack = require('@cypress/webpack-preprocessor')
+const options = {
+  webpackOptions,
+  watchOptions: {}
+}
 
 module.exports = (on, config) => {
-  // on('file:preprocessor', webpack({
-  //  webpackOptions: require('@vue/cli-service/webpack.config'),
-  //  watchOptions: {}
-  // }))
-
+  on('file:preprocessor', webpack(options))
   return Object.assign({}, config, {
     fixturesFolder: 'tests/e2e/fixtures',
     integrationFolder: 'tests/e2e/specs',
