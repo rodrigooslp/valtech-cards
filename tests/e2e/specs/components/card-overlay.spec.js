757e67ef109a8f2f6355e51b43842d2621d31267
Rodrigo Lopes <rodrigolopes@codilo.com.br>
2019-12-14 19:37:02 -0300

    Created the card overlay tests

---
+++ tests/e2e/specs/components/card-overlay.spec.js
@@ -0,0 +1,19 @@
+import CardOverlay from '@/components/elements/CardOverlay.vue'
+const mountVue = require('cypress-vue-unit-test')
+
+describe('CardOverlay', () => {
+  const template = `
+    <div id="app">
+      <card-overlay :class="modifier">{{ text }}</card-overlay>
+    </div>
+  `
+  const components = { CardOverlay }
+
+  beforeEach(mountVue({ template, components }))
+
+  it('the overlay should be visible', () => {
+    cy
+      .get('.card__overlay')
+      .should('be.visible')
+  })
+})
