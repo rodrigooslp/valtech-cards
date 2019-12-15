041b654010c2fb056b3c3b016f2f4ec0412ccfb8
Rodrigo Lopes <rodrigolopes@codilo.com.br>
2019-12-15 12:55:44 -0300

    Added tests to cover the animation on city content

--- tests/e2e/specs/components/blocks/card-city-content.spec.js
+++ tests/e2e/specs/components/blocks/card-city-content.spec.js
@@ -12,7 +12,14 @@ describe('CardCityContent', () => {
 
   beforeEach(mountVue({ template, data, components }))
 
-  it('the title passed should be rendered in the component', () => {
+  it('the overlay should be rendered and visible', () => {
+    cy
+      .get('.card__overlay')
+      .should('exist')
+      .should('be.visible')
+  })
+
+  it('the title passed should be rendered and visible', () => {
     const title = 'VENICE'
 
     Cypress.vue.title = title
@@ -20,11 +27,12 @@ describe('CardCityContent', () => {
     cy
       .get('.card__title')
       .should('exist')
+      .should('be.visible')
       .invoke('text')
       .should('eq', title)
   })
 
-  it('the subtitle passed should be rendered in the component', () => {
+  it('the subtitle passed should be rendered and visible', () => {
     const subtitle = 'ITALY'
 
     Cypress.vue.subtitle = subtitle
@@ -32,11 +40,12 @@ describe('CardCityContent', () => {
     cy
       .get('.card__subtitle')
       .should('exist')
+      .should('be.visible')
       .invoke('text')
       .should('eq', subtitle)
   })
 
-  it('the text passed should be rendered in the component', () => {
+  it('the text passed should be rendered and not visible', () => {
     const text = `
       Venice, the capital of northern Italy's Veneto region,
       is built on more than 100 small islands in a lagoon in the Adriatic Sea.
@@ -48,19 +57,84 @@ describe('CardCityContent', () => {
     cy
       .get('.card__description')
       .should('exist')
+      .should('not.be.visible')
       .invoke('text')
       .should('contain', text)
   })
 
-  it('the overlay should be rendered', () => {
-    cy
-      .get('.card__overlay')
-      .should('exist')
-  })
-
-  it('the button should be rendered', () => {
+  it('the button should be rendered and not visible', () => {
     cy
       .get('.card__button')
-      .should('exist')
+      .should('not.be.visible')
+  })
+
+  it('the description should become visible/invisible on mouseenter/mouseleave', () => {
+    cy
+      .get('.card__city-content')
+      .trigger('mouseenter')
+
+    cy
+      .get('.card__description')
+      .should('be.visible')
+
+    cy
+      .get('.card__city-content')
+      .trigger('mouseleave')
+
+    cy
+      .get('.card__description')
+      .should('not.be.visible')
+  })
+
+  it('the button should become visible/invisible on mouseenter/mouseleave', () => {
+    cy
+      .get('.card__city-content')
+      .trigger('mouseenter')
+
+    cy
+      .get('.card__button')
+      .should('be.visible')
+
+    cy
+      .get('.card__city-content')
+      .trigger('mouseleave')
+
+    cy
+      .get('.card__button')
+      .should('not.be.visible')
+  })
+
+  it('the header should move on mouseenter/mouseleave', () => {
+    let position
+
+    cy
+      .get('.card__city-header')
+      .then($header => {
+        position = $header.position()
+      })
+
+    cy
+      .get('.card__city-content')
+      .trigger('mouseenter')
+
+    cy.wait(1000)
+
+    cy
+      .get('.card__city-header')
+      .then($header => {
+        expect($header.position()).to.not.deep.equal(position)
+      })
+
+    cy
+      .get('.card__city-content')
+      .trigger('mouseleave')
+
+    cy.wait(1000)
+
+    cy
+      .get('.card__city-header')
+      .then($header => {
+        expect($header.position()).to.deep.equal(position)
+      })
   })
 })
